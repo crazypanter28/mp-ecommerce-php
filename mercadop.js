7c04102fbf040ae59bc3224e59f69bcd68c84515
alonso.montiel <alonso.montiel@digitaljaguar.com.mx>
2020-10-31 21:06:02 -0600

    JS

---
+++ mercadop.js
@@ -0,0 +1,7 @@
+
+
+$( document ).ready(function() {
+    alert("HOLA");
+    window.Mercadopago.setPublishableKey("APP_USR-158fff95-0bdf-4149-9abc-c8b0ac7f289f");
+    window.Mercadopago.getIdentificationTypes();
+});
