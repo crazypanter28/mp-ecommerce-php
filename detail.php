d236978021f7bdcbba7f9f45b08a4abcf7d042e1
alonso.montiel <alonso.montiel@digitaljaguar.com.mx>
2020-11-01 03:40:26 -0600

    asdasd

--- detail.php
+++ detail.php
@@ -1,62 +1,51 @@
-
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-// SDK de Mercado Pago
-require __DIR__ .  '/vendor/autoload.php';
-$url = $_SERVER['SERVER_NAME'];
-// Agrega credenciales
+// SDK
+require DIR .  '/vendor/autoload.php';
+$baseUrl = $_SERVER['SERVER_NAME'];
+//credentiales
 MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
-$payer = new MercadoPago\Payer();
-  $payer->name = "Lalo Landa";
-  $payer->surname = "Luevano";
-  $payer->email = "test_user_58295862@testuser.com";
-  $payer->phone = array(
-    "area_code" => "52",
-    "number" => "5549737300"
-  );
-  
-  $payer->address = array(
-    "street_name" => ":Insurgentes Sur",
-    "street_number" => 1602,
-    "zip_code" => "03940"
-  );
-
- 
-// Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
 
 // Crea un ítem en la preferencia
 $item = new MercadoPago\Item();
-$item->id = "1234";
-$item->title = $_POST['title'] ;
-$item->description = "Dispositivo móvil de Tienda e-commerce";
+$item->id = 1234;
+$item->title = $_POST['price'];
+$item->description = "​Dispositivo móvil de Tienda e-commerce";
+$item->picture_url = $_POST['img'];
 $item->quantity = $_POST['unit'];
 $item->unit_price = $_POST['price'];
-$item->external_reference = "alonso.locseg@gmail.com";
-
-$preference->payment_methods = array(
-    "excluded_payment_methods" => array(
-      array("id" => "amex")
-    ),
-    "excluded_payment_types" => array(
-      array("id" => "atm")
-    ),
-    "installments"  >=6
-  );
-
-$preference->back_urls = array(
-    "success" => $url."/success.php?",
-    "failure" => $url."/failure.php?",
-    "pending" => $url."/pending.php?"
-);
-$preference->notifications_url =  $url. "/notificacion";
-$preference->auto_return = "approved";
 $preference->items = array($item);
+// Crea Payer  en la preferencia
+$payer = new MercadoPago\Payer();
+$payer->name = 'Lalo';
+$payer->surname = 'Landa';
+$payer->email = 'test_user_58295862@testuser.com';
+$payer->phone  = array(
+            "area_code" => "52",
+            "number" => "5549737300"
+          );
+$payer->address  = array(
+            "zip_code" => "0394​ 0",
+            "street_name" => "Insurgentes Sur",
+            "street_number" => "1602"
+          );
+$preference->payer = $payer;
+// Crea Payment Method en la preferencia
+$preference->payment_methods = array(
+                "excluded_payment_methods" => array(array("id" => "amex")),
+                "excluded_payment_types" => array(array("id" => "atm")),
+                "installments" => 6
+              );
+$preference->external_reference = 'alonso.locseg@gmail.com';
+$preference->back_urls = array(
+            'success' => $baseUrl."/success.php", 
+            'pending' => $baseUrl."/pending.php",
+            'failure' => $baseUrl."/failure.php"
+          );
+$preference->notification_url = $baseUrl."/notification.php?source_news=webhooks";
+$preference->auto_return = 'approved';
 $preference->save();
-
 
 ?>
 <!DOCTYPE html>
