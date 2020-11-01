c16f7aeebfc186366bed6eed95dded9c705c10c0
alonso.montiel <alonso.montiel@digitaljaguar.com.mx>
2020-11-01 02:17:50 -0600

    asdas

--- detail.php
+++ detail.php
@@ -10,7 +10,7 @@ require __DIR__ .  '/vendor/autoload.php';
 MercadoPago\SDK::setAccessToken('APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948');
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 // Crea un objeto de preferencia
-$preference = new MercadoPago\Preference();
+
 
 // Crea un ítem en la preferencia
 $item = new MercadoPago\Item();
@@ -20,6 +20,7 @@ $item->description = "Dispositivo móvil de Tienda e-commerce";
 $item->quantity = $_POST['unit'];
 $item->unit_price = $_POST['price'];
 $item->external_reference = "alonso.locseg@gmail.com";
+
 $payer = new MercadoPago\Payer();
   $payer->name = "Lalo Landa";
   $payer->surname = "Luevano";
@@ -30,11 +31,11 @@ $payer = new MercadoPago\Payer();
   );
   
   $payer->address = array(
-    "street_name" => ":Insurgentes Sur",
+    "street_name" => "Insurgentes Sur",
     "street_number" => 1602,
     "zip_code" => "03940"
   );
-
+  $preference = new MercadoPago\Preference();
 $preference->$payer = $payer;
 
 $preference->payment_methods = array(
