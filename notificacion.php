31845de7599b9fdd9a2d569ed9f909b2b99af512
alonso.montiel <alonso.montiel@digitaljaguar.com.mx>
2020-11-01 04:53:25 -0600

    adsfadsf

--- notificacion.php
+++ notificacion.php
@@ -1,13 +1,27 @@
+
 <?php
 
  header("HTTP/1.1 200 OK");
+ require DIR .  '/vendor/autoload.php';
     MercadoPago\SDK::setAccessToken("APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181");
-    $content = $_POST;
-    $fp = fopen("notification.log","w");
-    chmod("notification.log", 0777);
+    $content = 'iniciamos\n';
+    $fp = fopen("notificacion.log","w");
+    chmod("notificacion.log", 0777);
     fwrite($fp,$content);
+    fwrite($fp,$_GET['link']);
+        fwrite($fp,$_POST['id']);
+        fwrite($fp,$_POST['live_mode']);
+        fwrite($fp,$_POST['type']);
+        fwrite($fp,$_POST['date_created']);
+        fwrite($fp,$_POST['application_id']);
+        fwrite($fp,$_POST['user_id']);
+        fwrite($fp,$_POST['version']);
+        fwrite($fp,$_POST['api_version']);
+        fwrite($fp,$_POST['action']);
+        fwrite($fp,$_POST['data']['id']);
+    fwrite($fp,'fin\n');
     fclose($fp);
-    switch($_POST["type"]) {
+    switch(isset($_POST["type"])) {
         case "payment":
             $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
             break;
@@ -23,4 +37,3 @@
     }
 
 ?>
-
