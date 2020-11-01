3649cbf58b3b3df24b2a0805ae7c055685b029b9
alonso.montiel <alonso.montiel@digitaljaguar.com.mx>
2020-11-01 03:42:30 -0600

    wewqe

--- detail.php
+++ detail.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // SDK
 require DIR .  '/vendor/autoload.php';
 $baseUrl = $_SERVER['SERVER_NAME'];
