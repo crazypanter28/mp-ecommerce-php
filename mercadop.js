00e84df7ea483152aa4644292dfd42c158ca36ad
alonso.montiel <alonso.montiel@digitaljaguar.com.mx>
2020-10-31 21:07:17 -0600

    OBTENER EL BANCO

--- mercadop.js
+++ mercadop.js
@@ -5,3 +5,33 @@ $( document ).ready(function() {
     window.Mercadopago.setPublishableKey("APP_USR-158fff95-0bdf-4149-9abc-c8b0ac7f289f");
     window.Mercadopago.getIdentificationTypes();
 });
+
+document.getElementById('cardNumber').addEventListener('change', guessPaymentMethod);
+
+function guessPaymentMethod(event) {
+   let cardnumber = document.getElementById("cardNumber").value;
+   if (cardnumber.length >= 6) {
+       let bin = cardnumber.substring(0,6);
+       window.Mercadopago.getPaymentMethod({
+           "bin": bin
+       }, setPaymentMethod);
+   }
+};
+
+function setPaymentMethod(status, response) {
+   if (status == 200) {
+       let paymentMethod = response[0];
+       document.getElementById('paymentMethodId').value = paymentMethod.id;
+
+       if(paymentMethod.additional_info_needed.includes("issuer_id")){
+           getIssuers(paymentMethod.id);
+       } else {
+           getInstallments(
+               paymentMethod.id,
+               document.getElementById('transactionAmount').value
+           );
+       }
+   } else {
+       alert(`payment method info error: ${response}`);
+   }
+}
