bcf396df6e509f2bbf4a3d4785fb2869044d548e
alonso.montiel <alonso.montiel@digitaljaguar.com.mx>
2020-11-01 04:54:37 -0600

    asdasd

--- notificacion.php
+++ notificacion.php
@@ -2,7 +2,7 @@
 <?php
 
  header("HTTP/1.1 200 OK");
- require DIR .  '/vendor/autoload.php';
+ require __DIR__ .  '/vendor/autoload.php';
     MercadoPago\SDK::setAccessToken("APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181");
     $content = 'iniciamos\n';
     $fp = fopen("notificacion.log","w");
