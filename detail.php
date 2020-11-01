27c842538d79b25e9bf0bdb8b4751e8aa60df02e
alonso.montiel <alonso.montiel@digitaljaguar.com.mx>
2020-11-01 02:22:00 -0600

    asdsad

--- detail.php
+++ detail.php
@@ -9,18 +9,6 @@ require __DIR__ .  '/vendor/autoload.php';
 // Agrega credenciales
 MercadoPago\SDK::setAccessToken('APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948');
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
-// Crea un objeto de preferencia
-
-
-// Crea un ítem en la preferencia
-$item = new MercadoPago\Item();
-$item->id = "1234";
-$item->title = $_POST['title'] ;
-$item->description = "Dispositivo móvil de Tienda e-commerce";
-$item->quantity = $_POST['unit'];
-$item->unit_price = $_POST['price'];
-$item->external_reference = "alonso.locseg@gmail.com";
-
 $payer = new MercadoPago\Payer();
   $payer->name = "Lalo Landa";
   $payer->surname = "Luevano";
@@ -31,13 +19,24 @@ $payer = new MercadoPago\Payer();
   );
   
   $payer->address = array(
-    "street_name" => "Insurgentes Sur",
+    "street_name" => ":Insurgentes Sur",
     "street_number" => 1602,
     "zip_code" => "03940"
   );
-  $preference = new MercadoPago\Preference();
-$preference->$payer = array($payer);;
 
+ 
+// Crea un objeto de preferencia
+$preference = new MercadoPago\Preference();
+
+// Crea un ítem en la preferencia
+$item = new MercadoPago\Item();
+$item->id = "1234";
+$item->title = $_POST['title'] ;
+$item->description = "Dispositivo móvil de Tienda e-commerce";
+$item->quantity = $_POST['unit'];
+$item->unit_price = $_POST['price'];
+$item->external_reference = "alonso.locseg@gmail.com";
+$preference->$payer = $payer;
 $preference->payment_methods = array(
     "excluded_payment_methods" => array(
       array("id" => "amex")
