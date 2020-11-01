fe29d15abdbd0ec61022da701f2796585ef9f596
alonso.montiel <alonso.montiel@digitaljaguar.com.mx>
2020-10-31 21:19:29 -0600

    proces

--- mercadop.js
+++ mercadop.js
@@ -1,7 +1,6 @@
 
 
 $( document ).ready(function() {
-    alert("HOLA");
     window.Mercadopago.setPublishableKey("APP_USR-158fff95-0bdf-4149-9abc-c8b0ac7f289f");
     window.Mercadopago.getIdentificationTypes();
 });
@@ -35,3 +34,78 @@ function setPaymentMethod(status, response) {
        alert(`payment method info error: ${response}`);
    }
 }
+
+function getIssuers(paymentMethodId) {
+    window.Mercadopago.getIssuers(
+        paymentMethodId,
+        setIssuers
+    );
+ }
+ 
+ function setIssuers(status, response) {
+    if (status == 200) {
+        let issuerSelect = document.getElementById('issuer');
+        response.forEach( issuer => {
+            let opt = document.createElement('option');
+            opt.text = issuer.name;
+            opt.value = issuer.id;
+            issuerSelect.appendChild(opt);
+        });
+ 
+        getInstallments(
+            document.getElementById('paymentMethodId').value,
+            document.getElementById('transactionAmount').value,
+            issuerSelect.value
+        );
+    } else {
+        alert(`issuers method info error: ${response}`);
+    }
+ }
+
+ function getInstallments(paymentMethodId, transactionAmount, issuerId){
+    window.Mercadopago.getInstallments({
+        "payment_method_id": paymentMethodId,
+        "amount": parseFloat(transactionAmount),
+        "issuer_id": issuerId ? parseInt(issuerId) : undefined
+    }, setInstallments);
+ }
+ 
+ function setInstallments(status, response){
+    if (status == 200) {
+        document.getElementById('installments').options.length = 0;
+        response[0].payer_costs.forEach( payerCost => {
+            let opt = document.createElement('option');
+            opt.text = payerCost.recommended_message;
+            opt.value = payerCost.installments;
+            document.getElementById('installments').appendChild(opt);
+        });
+    } else {
+        alert(`installments method info error: ${response}`);
+    }
+ }
+
+ doSubmit = false;
+document.getElementById('paymentForm').addEventListener('submit', getCardToken);
+function getCardToken(event){
+   event.preventDefault();
+   if(!doSubmit){
+       let $form = document.getElementById('paymentForm');
+       window.Mercadopago.createToken($form, setCardTokenAndPay);
+       return false;
+   }
+};
+
+function setCardTokenAndPay(status, response) {
+   if (status == 200 || status == 201) {
+       let form = document.getElementById('paymentForm');
+       let card = document.createElement('input');
+       card.setAttribute('name', 'token');
+       card.setAttribute('type', 'hidden');
+       card.setAttribute('value', response.id);
+       form.appendChild(card);
+       doSubmit=true;
+       form.submit();
+   } else {
+       alert("Verify filled data!\n"+JSON.stringify(response, null, 4));
+   }
+};
---
+++ process_payment.php
@@ -0,0 +1,32 @@
+<?php
+    require_once 'vendor/autoload.php';
+
+    MercadoPago\SDK::setAccessToken("APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948");
+    echo("aaaaaaaaaaaaa");
+    die();
+    $payment = new MercadoPago\Payment();
+    $payment->transaction_amount = (float)$_POST['transactionAmount'];
+    $payment->token = $_POST['token'];
+    $payment->description = $_POST['description'];
+    $payment->installments = (int)$_POST['installments'];
+    $payment->payment_method_id = $_POST['paymentMethodId'];
+    $payment->issuer_id = (int)$_POST['issuer'];
+
+    $payer = new MercadoPago\Payer();
+    $payer->email = $_POST['email'];
+    $payer->identification = array( 
+        "type" => $_POST['docType'],
+        "number" => $_POST['docNumber']
+    );
+    $payment->payer = $payer;
+
+    $payment->save();
+
+    $response = array(
+        'status' => $payment->status,
+        'status_detail' => $payment->status_detail,
+        'id' => $payment->id
+    );
+    echo json_encode($response);
+
+?>
