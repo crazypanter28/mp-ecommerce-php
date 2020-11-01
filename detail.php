747668925d51653f166f4b2d261825b3379a56da
alonso.montiel <alonso.montiel@digitaljaguar.com.mx>
2020-10-31 22:40:32 -0600

    acces token

--- detail.php
+++ detail.php
@@ -461,7 +461,8 @@
 require __DIR__ .  '/vendor/autoload.php';
 
 // Agrega credenciales
-MercadoPago\SDK::setAccessToken('PROD_ACCESS_TOKEN');
+MercadoPago\SDK::setAccessToken('APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948
+');
 
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
